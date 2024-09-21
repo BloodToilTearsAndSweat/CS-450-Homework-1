f21222f9b0925aa0d3c4150cf31ec047d17a781b
BloodToilTearsAndSweat <25108818+BloodToilTearsAndSweat@users.noreply.github.com>
2024-09-20 22:17:13 -0400

    initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+// Evan Friedman
+// CS 450: Software Engineering
+// Homework 1
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello, World" << endl;
+    return 0;
+}
